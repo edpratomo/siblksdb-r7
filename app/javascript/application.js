2aa07c924fa5ec75e793647550242307226d637b
Edwin Pratomo <edpratomo@cpan.org>
2024-12-03 09:12:31 +0700

    initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,12 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import * as bootstrap from "bootstrap"
+import "@fortawesome/fontawesome-free/js/all"
+
+import "./add_jquery"
+import "admin-lte"
+
+// import "admin-lte/dist/js/adminlte";
+
+// require('admin-lte');
