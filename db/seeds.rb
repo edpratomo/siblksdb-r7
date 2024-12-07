cb125947faf90980e432172d286ddd0f8adc13e5
Edwin Pratomo <edpratomo@cpan.org>
2024-12-07 15:54:01 +0700

    fixed db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 fullname, username, email, password = ENV['DEVISE_FIRST_USER'].split(",").map(&:strip)
-user = User.new(fullname: fullname, username: username, email: email, password: password, password_confirmation: password)
+group = Group.find_by(name: "sysadmin")
+raise "Group not found" unless group
+user = User.new(fullname: fullname, username: username, group: group, email: email, password: password, password_confirmation: password)
 user.save(validate: false)
