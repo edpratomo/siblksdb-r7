b96ead9244e5b875fef018bb00745c023d554b56
Edwin Pratomo <edpratomo@cpan.org>
2024-12-11 02:33:29 +0700

    added moment.js locale id

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,9 +8,18 @@ import "./add_jquery"
 import "admin-lte"
 
 import moment from "moment/dist/moment"
+import "moment/dist/locale/id"
 window.moment = moment;
 
 import toastr from "toastr"
 window.toastr = toastr;
 
-require "tempusdominus-bootstrap-4/build/js/tempusdominus-bootstrap-4"
+// import "tempusdominus-bootstrap-4/build/js/tempusdominus-bootstrap-4"
+
+require("tempusdominus-bootstrap-4");
+
+// console.log($.fn.datetimepicker);
+
+document.addEventListener("turbo:load", () => {
+  $('#reservationdatetime').datetimepicker({ icons: { time: 'far fa-clock' } });
+});
