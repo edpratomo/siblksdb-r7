e6ee40bbe18d7bb286db8df1b108cb0a52104da3
Edwin Pratomo <edpratomo@cpan.org>
2024-12-13 22:21:45 +0700

    fixed course fee submit

--- app/controllers/fees_controller.rb
+++ app/controllers/fees_controller.rb
@@ -3,11 +3,12 @@ class FeesController < ApplicationController
 
   # GET /fees or /fees.json
   def index
-    #@fee = Fee.new
     @fee = params[:type].constantize.new
     @fee.type = params[:type]
     @courses_options = Course.options_for_select
     
+    Rails.logger.debug("controller: #{controller_name}")
+    Rails.logger.debug("request_path: #{request.path}")
     @fees = Fee.where(:type => params[:type])
   end
 
@@ -29,10 +30,11 @@ class FeesController < ApplicationController
   # POST /fees or /fees.json
   def create
     @fee = Fee.new(fee_params)
+    @fee.amount = @fee.amount.to_i
 
     respond_to do |format|
       if @fee.save
-        format.html { redirect_to @fee, notice: "Fee was successfully created." }
+        format.html { redirect_to request.path, notice: "Fee was successfully created." }
         format.json { render :show, status: :created, location: @fee }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -72,6 +74,21 @@ class FeesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def fee_params
-      params.fetch(:fee, {})
+      #params.fetch(:fee, {})
+      Rails.logger.debug("params type: #{params[:type]}")
+      permit_map = {
+        "CourseFee" => [:course_id, :amount, :active_since, :type],
+        "BookUniformFee" => [:program_id, :amount, :active_since, :type],
+        "AdmissionFee" => [:amount, :active_since, :type]
+      }
+      %w[RegCourseFee IntCourseFee ExtCourseFee].each do |course_fee|
+        permit_map[course_fee] = permit_map["CourseFee"]
+      end
+      allowed_params = permit_map[params[:type]]
+      Rails.logger.debug("permit_map: #{permit_map}")
+      Rails.logger.debug("allowed_params: #{allowed_params}")
+      if allowed_params
+        params.require(params[:type].underscore.to_sym).permit(*allowed_params)
+      end
     end
 end
