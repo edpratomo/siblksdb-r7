693ac88331b49c04d0731042fe115423a6463275
Edwin Pratomo <edpratomo@cpan.org>
2025-01-15 18:47:05 +0700

    added store_location method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,19 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # http://stackoverflow.com/questions/2139996/how-to-redirect-to-previous-page-in-ruby-on-rails
+  def store_location
+    return unless request.get?
+    if (controller_name != "sessions" and
+        controller_name != "password_resets" and
+        controller_name != "users" and
+        !request.xhr?) # don't store ajax calls
+      session[:return_to] = request.fullpath
+    end
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+  end
+
 end
