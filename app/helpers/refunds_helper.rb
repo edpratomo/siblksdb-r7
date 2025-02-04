619dcc89a716f7ed590efe80b40062c9ad44ec55
Edwin Pratomo <edpratomo@cpan.org>
2025-02-05 02:23:18 +0700

    fixed refunds badge

--- app/helpers/refunds_helper.rb
+++ app/helpers/refunds_helper.rb
@@ -1,6 +1,14 @@
 module RefundsHelper
   def outstanding_refunds_count
     or_c = Refund.already_paid('0').size
-    or_c > 0 ? or_c : ''
+    or_c > 0 ? or_c.to_s : ''
+  end
+
+  def refunds_count_badge
+    if outstanding_refunds_count.empty?
+      raw(%Q(<span id="refunds-count"></span>))
+    else
+      raw(%Q(<span id="refunds-count" class="right badge badge-danger">#{outstanding_refunds_count}</span>))
+    end
   end
 end
