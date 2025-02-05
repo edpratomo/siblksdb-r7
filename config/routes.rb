a78b8280630bbae6d152075df2b726e8a514be99
Edwin Pratomo <edpratomo@cpan.org>
2025-02-05 12:43:12 +0700

    rails g scaffold Admin::Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
   resources :refunds do
     collection do
       get ':invoice_id/new' => 'refunds#new', as: "new"
